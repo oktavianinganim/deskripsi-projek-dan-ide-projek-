9900231683c580d3c776783968651d207fc0e038
ani <127914193+oktavianinganim@users.noreply.github.com>
2023-06-20 21:18:19 +0800

    Add files via upload

---
+++ deskripsi .php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h3>DEFENISI DATABASE</h3>
+        <img src="conoth.jpeg"width="500"height="550">
+
+
+<h5>
+Dalam contoh di atas, script akan membuat database "calculator" jika belum ada. Kemudian, akan digunakan database tersebut. 
+Setelah itu, tabel "calculations" akan dibuat jika belum ada.</h5><h5> Tabel tersebut memiliki kolom "id" sebagai primary key dengan tipe data INT dan auto_increment, kolom "calculation" dengan tipe data VARCHAR(50) untuk menyimpan operasi perhitungan, dan kolom "result" dengan tipe data FLOAT untuk menyimpan hasil perhitungan.
+</h6
+5>
+        
+
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DESKRIPSI</title>
+</head>
+<body>
+    <h1>IDE PROJEK</h1>
+    <h6>
+Ide proyek dari aplikasi ini adalah menciptakan sebuah aplikasi kalkulator sederhana dengan fitur untuk melakukan operasi matematika dasar seperti penjumlahan, pengurangan, perkalian, dan pembagian.</h6><h6> Aplikasi ini juga menggunakan database untuk menyimpan hasil perhitungan pengguna.</h6>
+<h5>Tujuan utama dari proyek ini adalah menyediakan sebuah alat kalkulasi yang mudah digunakan dengan antarmuka yang intuitif. Pengguna dapat memasukkan angka dan operator matematika untuk melakukan perhitungan, dan hasilnya akan ditampilkan secara real-time.</h5> Aplikasi juga akan menyimpan hasil perhitungan ke dalam database untuk referensi dan penggunaan di masa mendatang.
+Selain itu, aplikasi ini memiliki fitur responsif sehingga dapat diakses melalui berbagai perangkat seperti desktop, tablet, dan smartphone.<h5> Pengguna dapat mengaksesnya melalui browser web tanpa memerlukan instalasi khusus.</h6>
+<h5>Dengan adanya fitur database, pengguna dapat melihat riwayat perhitungan yang telah dilakukan sebelumnya. Hal ini memungkinkan pengguna untuk melacak dan mengulang perhitungan yang telah dilakukan sebelumnya.
+Proyek ini dapat bermanfaat bagi pengguna yang membutuhkan alat kalkulasi sederhana, seperti dalam konteks pembelajaran matematika, penghitungan keuangan, atau keperluan sehari-hari lainnya.</h5>
+
+
+<h5>Teknologi yang Digunakan:</h5>
+<h5>1.Bahasa Pemrograman: PHP, JavaScript, HTML, CSS.<</h5>
+<h5>2.Framework: Tidak menggunakan framework, namun menggunakan library jQuery untuk mempermudah manipulasi DOM dan pengiriman data melalui AJAX.</h5>
+</body>
+</h6>
+
+
+
+<h3>
+    DESKRIPSI PROJECT</h3>
+<h5>Nama Proyek: Kalkulator Sederhana</h5>
+<h5>Deskripsi Singkat: Aplikasi kalkulator sederhana yang memungkinkan pengguna melakukan operasi matematika dasar dan menyimpan hasil perhitungan ke dalam database.
+Fitur yang Diharapkan:</h5>
+1.Tampilan antarmuka yang intuitif: Desain antarmuka yang user-friendly dengan tombol dan field input yang jelas.
+<h5>2.Operasi Matematika Dasar: Memungkinkan pengguna untuk melakukan penjumlahan, pengurangan, perkalian, dan pembagian.</h5>
+<h5>3.Responsif: Aplikasi dapat diakses dan berfungsi dengan baik pada berbagai perangkat seperti desktop, tablet, dan smartphone.</h5>
+</h5>4.Keamanan: Melakukan validasi input pengguna untuk memastikan bahwa hanya angka dan operator yang valid yang dapat dimasukkan.</h5>
+
+<h4>Aplikasi kalkulator sederhana ini dirancang untuk membantu pengguna melakukan operasi matematika dasar seperti penjumlahan, pengurangan, perkalian, dan pembagian. Pengguna akan memasukkan angka dan operator melalui antarmuka yang intuitif.</h4> <h5>Setelah pengguna mengklik tombol "Hitung", hasil perhitungan akan ditampilkan secara langsung.
+</h5>
+<h5>Antarmuka aplikasi ini responsif, sehingga dapat diakses melalui berbagai perangkat seperti desktop, tablet, dan smartphone. Validasi input juga dilakukan untuk memastikan bahwa pengguna hanya memasukkan angka dan operator yang valid.
+
+<h5>Dalam proyek ini, kita tidak menggunakan database untuk menyimpan riwayat perhitungan.</h5> Aplikasi ini hanya fokus pada fitur perhitungan matematika dasar dan menampilkan hasil secara langsung kepada pengguna.</h5>
+
+
+
+
+
+</h6>
+</html>
